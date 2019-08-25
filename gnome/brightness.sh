08d15e90f2c38f1ee15fc2dd422e24b8e366bb2f
Cody Hodges <cody35367@gmail.com>
2019-08-24 23:57:45 -0500

    Add new brightness script.

---
+++ gnome/brightness.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+BRIGHTNESS_PERCENT=20
+gdbus call --session --dest org.gnome.SettingsDaemon.Power --object-path /org/gnome/SettingsDaemon/Power --method org.freedesktop.DBus.Properties.Set org.gnome.SettingsDaemon.Power.Screen Brightness "<int32 ${BRIGHTNESS_PERCENT}>"
