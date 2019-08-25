82e4789af2c353db1e8f3599760578a2adbcb02e
Cody Hodges <cody35367@gmail.com>
2019-08-25 00:02:49 -0500

    Add a sleep on brightness

--- gnome/brightness.sh
+++ gnome/brightness.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 BRIGHTNESS_PERCENT=20
+SLEEP_TIME=10
+sleep ${SLEEP_TIME}
 gdbus call --session --dest org.gnome.SettingsDaemon.Power --object-path /org/gnome/SettingsDaemon/Power --method org.freedesktop.DBus.Properties.Set org.gnome.SettingsDaemon.Power.Screen Brightness "<int32 ${BRIGHTNESS_PERCENT}>"
