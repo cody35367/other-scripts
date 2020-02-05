7f03d41230f927bbc2a5eec59398775426113160
Cody Hodges <cody35367@gmail.com>
2020-02-04 23:29:37 -0600

    Add new files for making Veloren shortcut

---
+++ linux-gaming/Veloren.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd ~/Games/veloren
+./veloren-voxygen
---
+++ linux-gaming/make-veloren-desktop-file.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+python3 ../gnome/gen_desktop_file.py ./Veloren.sh ~/.local/share/applications/Veloren.desktop
