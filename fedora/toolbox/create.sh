9b9d4eb7f115ba8fdf52382779e661d7c24f06d2
Cody Hodges <cody35367@gmail.com>
2020-05-01 18:17:03 -0500

    Update with padding zeros so that it support sorting correctly on values above 9

--- fedora/toolbox/create.sh
+++ fedora/toolbox/create.sh
@@ -2,15 +2,22 @@
 
 IMAGE_NAME="custom-toolbox"
 CONTAINER_NAME_PREFIX="ct"
+# Pad zeros so that we support up to 999 to conatiners.
+PRINTF_FORMAT="%03d"
+MAX_CONTAINER_NUMBER=999
 
 LAST_CONTAINER=$(podman ps -a --format="{{.Names}}" --filter="name=${CONTAINER_NAME_PREFIX}[0-9]+" | sort | tail -n 1)
 CONTAINER_NAME=""
 if [[ -n ${LAST_CONTAINER} ]]; then
-    LAST_NUMBER=$(echo "${LAST_CONTAINER}" | grep -oP '[0-9]+')
-    ((LAST_NUMBER++))
-    CONTAINER_NAME="${CONTAINER_NAME_PREFIX}${LAST_NUMBER}"
+    CONTAINER_NUMBER=$(echo "${LAST_CONTAINER}" | grep -oP '[0-9]+')
+    ((CONTAINER_NUMBER++))
+    if [[ ${CONTAINER_NUMBER} -gt ${MAX_CONTAINER_NUMBER} ]]; then
+        echo "Container number \"${CONTAINER_NUMBER}\" is not supported."
+        exit 1
+    fi
+    CONTAINER_NAME="${CONTAINER_NAME_PREFIX}$(printf ${PRINTF_FORMAT} ${CONTAINER_NUMBER})"
 else
-    CONTAINER_NAME="${CONTAINER_NAME_PREFIX}1"
+    CONTAINER_NAME="${CONTAINER_NAME_PREFIX}$(printf ${PRINTF_FORMAT} 1)"
 fi
 
 cd "$(dirname "$0")"
