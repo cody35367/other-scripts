95f0e1df5a71a837ddaa0fc9ba1446dfca3db8fd
Cody Hodges <cody35367@gmail.com>
2020-05-01 21:41:15 -0500

    Update install script to get the latest oc to download the beta and then dymanically extract and install.

--- okd/install-tools-in-user-home.sh
+++ okd/install-tools-in-user-home.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+set -e
+
 USER_HOME_BIN_PATH="${HOME}/.local/bin/"
 USER_DOWNLOADS_DIR="${HOME}/Downloads/installed/"
-OC_DOWNLOAD_URL="https://github.com/openshift/okd/releases/download/4.4.0-0.okd-2020-04-21-163702-beta4/openshift-client-linux-4.4.0-0.okd-2020-04-21-163702-beta4.tar.gz"
-OC_DOWNLOAD_FILE="openshift-client-linux-4.4.0-0.okd-2020-04-21-163702-beta4.tar.gz"
+LATEST_OC_DOWNLOAD_URL="https://mirror.openshift.com/pub/openshift-v4/clients/oc/latest/linux/oc.tar.gz"
+LATEST_OC_DOWNLOAD_FILE="oc.tar.gz"
+REGISTRY_URL="quay.io/openshift/okd:4.4.0-0.okd-2020-04-21-163702-beta4"
 
 mkdir -pv ${USER_DOWNLOADS_DIR}
-curl -L ${OC_DOWNLOAD_URL} -o ${USER_DOWNLOADS_DIR}${OC_DOWNLOAD_FILE}
-tar -xzf ${USER_DOWNLOADS_DIR}${OC_DOWNLOAD_FILE} -C ${USER_HOME_BIN_PATH} oc kubectl
+curl -L ${LATEST_OC_DOWNLOAD_URL} -o ${USER_DOWNLOADS_DIR}${LATEST_OC_DOWNLOAD_FILE}
+tar -xzf ${USER_DOWNLOADS_DIR}${LATEST_OC_DOWNLOAD_FILE} -C ${USER_HOME_BIN_PATH} oc
+cd ${USER_DOWNLOADS_DIR}
+oc adm release extract --tools ${REGISTRY_URL}
+if ! sha256sum -c ./sha256sum.txt; then
+    echo "Error verifying checksums."
+    exit 1
+fi
+CURRENT_RELEASE_CLIENT_TAR=$(cat ./sha256sum.txt | awk '{print $2}' | grep client)
+CURRENT_RELEASE_INSTALL_TAR=$(cat ./sha256sum.txt | awk '{print $2}' | grep install)
+rm -v ./sha256sum.txt ./release.txt
+echo
+echo "Installed the following in \"${USER_HOME_BIN_PATH}\":"
+tar -xvzf ${USER_DOWNLOADS_DIR}${CURRENT_RELEASE_CLIENT_TAR} -C ${USER_HOME_BIN_PATH} oc kubectl
+tar -xvzf ${USER_DOWNLOADS_DIR}${CURRENT_RELEASE_INSTALL_TAR} -C ${USER_HOME_BIN_PATH} openshift-install
