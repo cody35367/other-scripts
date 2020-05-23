75c052f4f7bf19cc7ec8fa2fd56f80bcc4c5d1e7
Cody Hodges <cody35367@gmail.com>
2020-05-23 02:22:25 -0500

    Update to chrome instead of Chromium after some issues.

--- fedora/base-install.sh
+++ fedora/base-install.sh
@@ -78,9 +78,8 @@ sudo dnf install -y \
     https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-sudo dnf check-update --refresh
+sudo dnf install -y https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
 sudo dnf install -y \
-    chromium \
     vim \
     git \
     gitk \
