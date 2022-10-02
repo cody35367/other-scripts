e55fee4d6350dc7cc19aed87f221c2e1d6f5aca3
Cody Hodges <cody35367@gmail.com>
2022-10-02 01:21:05 -0500

    Fixes for samba add/remove

--- home-server/samba/add-samba-user.sh
+++ home-server/samba/add-samba-user.sh
@@ -8,5 +8,5 @@ fi
 HOME_DIR_BASE="/p1/shares/"
 COMMON_GROUPS="family"
 
-sudo useradd -m -b ${HOME_DIR_BASE}$1 -G ${COMMON_GROUPS} -s /bin/bash -U $1
+sudo useradd -m -d ${HOME_DIR_BASE}$1 -G ${COMMON_GROUPS} -s /bin/bash -U $1
 sudo smbpasswd -a $1
--- home-server/samba/remove-samba-user.sh
+++ home-server/samba/remove-samba-user.sh
@@ -9,4 +9,4 @@ HOME_DIR_BASE="/p1/shares/"
 
 sudo smbpasswd -x $1
 sudo userdel $1
-echo "Remember to delete the home dir at ${HOME_DIR_BASE}%1 if you are done with it."
+echo "Remember to delete the home dir at ${HOME_DIR_BASE}$1 if you are done with it."
