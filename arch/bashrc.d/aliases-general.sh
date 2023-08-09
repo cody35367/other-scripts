ccdea8397b610ea3362228be2697c357f8442192
Cody Hodges <cody35367@gmail.com>
2023-08-08 21:58:08 -0500

    Launching with disable GPU to reduce lag on wayland for whatever reason.

--- arch/bashrc.d/aliases-general.sh
+++ arch/bashrc.d/aliases-general.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 alias update="${HOME}/repos/other-scripts/arch/full-upgrade.sh"
+alias code="${HOME}/Applications/VSCode-linux-x64/bin/code --disable-gpu"
---
+++ arch/bashrc.d/path-general.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# if ! [[ "$PATH" =~ "$HOME/Applications/VSCode-linux-x64/bin/:" ]]
+# then
+#     PATH="$HOME/Applications/VSCode-linux-x64/bin/:$PATH"
+# fi
+# export PATH
