53c10c2b58f6647bf30fe967e5e7c0be6792996e
Cody Hodges <cody35367@gmail.com>
2023-09-10 22:47:34 -0500

    Seems to work a bit better.

--- arch/bashrc.d/aliases-general.sh
+++ arch/bashrc.d/aliases-general.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 alias update="${HOME}/repos/other-scripts/arch/full-upgrade.sh"
-alias code="${HOME}/Applications/VSCode-linux-x64/bin/code --disable-gpu"
+alias code="${HOME}/Applications/VSCode-linux-x64/bin/code --enable-features=UseOzonePlatform --ozone-platform=wayland"
