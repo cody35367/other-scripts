a37ee14ff7984a7f6c194d711e64081356fa2f6d
Cody Hodges <cody35367@gmail.com>
2023-09-14 12:04:03 -0500

    Upgrade to backport packages.

--- debian/full-upgrade.sh
+++ debian/full-upgrade.sh
@@ -4,8 +4,12 @@ if [[ ! -z ${SUDO_USER} ]]; then
   echo "Please do not run as sudo!"
   exit 1
 fi
+
+. /etc/os-release
+
 sudo apt update
 sudo apt full-upgrade -y
+sudo apt -t ${VERSION_CODENAME}-backports full-upgrade -y
 sudo apt autoremove -y
 if [ -f /var/run/reboot-required ]; then
   echo
